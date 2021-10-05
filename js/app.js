531d89998667f88ce7c8a042f773c28b60f6f44d
Milanl02 <89406051+Milanl02@users.noreply.github.com>
2021-10-04 20:27:59 -0400

    Hopefully Last

--- js/app.js
+++ js/app.js
@@ -3,26 +3,32 @@ var user_play=document.body.querySelector(".dragons")
 var dragon_health=10;
 var knights_health=5;
 
-    while(dragon_health>0&&knights_health>0) {
-        // var dragon_health=10;
-        // var knights_health=5;
-        var user_damage = Number(prompt("How many times do you want to hit the dragon? (Choose a number up to 5)"));
-
-        var user_dealt_damage = Math.floor(Math.random() * user_damage) + 1;
-        if (user_damage > 5) {
-            var dragon_health_now = dragon_health - 1;
+    while(dragon_health>0||knights_health>0){
+        var question = Number(prompt("How many times do you want to hit the dragon? (Choose a number up to 5)"));
+        var user_dealt_damage = Math.floor(Math.random() * question) + 1;
+        if (user_dealt_damage > 5) {
+            dragon_health = dragon_health - 1;
         } else {
-            dragon_health_now = dragon_health - user_dealt_damage;
+            dragon_health = dragon_health - user_dealt_damage
         }
+        //Everything above works
+        var dragon_dealt_damage = Math.floor(Math.random() * 2) + 1;
+        knights_health = knights_health - dragon_dealt_damage;
+
+        console.log(dragon_health)
+        console.log(knights_health)
+
     }
 
-if (dragon_health > 0) {
-    var dragon_dealt_damage = Math.floor(Math.random() * 3) + 1;
-    var knights_health_now = knights_health - dragon_dealt_damage
-} else {
-    user_play.innerHTML = "You defeated the dragon, you win!";
+if (knights_health<=0){
+    user_play.innerHTML = "The dragon killed you!"
+}else if (dragon_health<=0){
+    user_play.innerHTML="You killed it."
 }
 
-if(knights_health<0){
-    user_play.innerHTML = "You lost to the dragon, you lose.";
-}
+
+
+
+
+
+   
